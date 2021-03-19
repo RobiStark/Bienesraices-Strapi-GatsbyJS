b50b81082c47bd42d8d4f13a4760dc56f1c929ac
Roberto <roberto.websistem@hotmail.com>
2021-03-18 22:14:44 -0600

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '35a5bb159ba3c982955b9d74719578fe'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f1db51ca-0deb-4bc6-ab2c-01cf0d909866'
+};
